4dcac4ce5339b59a0041336b74161f973e8374ef
3075375046 <3075375046@qq.com>
2024-06-17 15:32:22 +0800

    Site updated: 2024-06-17 15:32:22

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/06/14/hello-world/","2024/06/14/黄子秦/"];function toRandomPost(){
+var posts=["2024/06/14/黄子秦/","2024/06/17/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
