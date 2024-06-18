d246074cb6101a29d91bf5cb6528b5e1baf1d732
3075375046 <3075375046@qq.com>
2024-06-18 21:22:53 +0800

    Site updated: 2024-06-18 21:22:46

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/06/14/黄子秦/","2024/06/17/hello-world/"];function toRandomPost(){
+var posts=["2024/06/14/黄子秦/","2024/06/17/hello-world/","2024/06/18/黑奴市场/","2024/06/18/修改头像/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
